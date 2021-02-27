f419469dc6c0e8a908abae18a317b7aaa215bc20
Amanda Peterson <amanda.peterson938@gmail.com>
2021-02-27 09:55:25 -0600

    Added an UISegmentedControl so that it will be easy to switch between Signing In and Creating an Account. Connected the segmented control to a new view so you didn't have to. Rounded the submission button because I felt it looked better that way.

--- Form2FlyUI/ViewController.swift
+++ Form2FlyUI/ViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var signOnBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        signOnBtn.layer.cornerRadius = 12
     }
-
-
+    
 }
 
