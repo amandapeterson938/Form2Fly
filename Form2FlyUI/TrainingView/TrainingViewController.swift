8e12889137d21571b2d17ffc07996631b745a355
Amanda Peterson <amanda.peterson938@gmail.com>
2021-02-28 15:12:47 -0600

    Still need to do training view and revisit the professional list.

---
+++ Form2FlyUI/TrainingView/TrainingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TrainingViewController.swift
+//  Form2FlyUI
+//
+//  Created by Amanda Peterson on 2/28/21.
+//
+
+import UIKit
+
+class TrainingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
